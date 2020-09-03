e262c1f5efaaa9a07f38c3c6e79592c1e8552fe3
vaughan189 <vaughan188@gmail.com>
2020-09-03 16:42:21 +0530

    fixed eslint issue with describe and it

--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,12 +1,12 @@
 import 'mocha';
 import { expect } from 'chai';
 
-describe('bar', () => {
-  it('sync function returns true', () => {
+describe('bar', function () {
+  it('sync function returns true', function () {
     expect(true).to.be.true;
   });
 
-  it('async function returns true', async () => {
+  it('async function returns true', async function () {
     expect(true).to.be.true;
   });
 });
